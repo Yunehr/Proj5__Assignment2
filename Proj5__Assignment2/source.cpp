22b8cefd4d51ca3f99ce59a7c991859b3b4597c2
Ryan H. <Ryanhackbart@gmail.com>
2026-09-15 14:07:52 -0400

    Initial Source Code - Created STUDENT_DATA Struct - reads StudentData.txt and stores student first/last name to vector space called `students`

--- Proj5__Assignment2/source.cpp
+++ Proj5__Assignment2/source.cpp
@@ -1,5 +1,46 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+struct STUDENT_DATA
+{
+	std::string firstname;
+	std::string lastname;
+};
 
 int main() {
+	std::ifstream inputFile("../StudentData.txt");
+	if (!inputFile.is_open()) {
+		std::cerr << "Error: Could not open the file!" << std::endl;
+		return 1;
+	}
+
+	//make students vector to store student data
+	std::vector<STUDENT_DATA> students;
+
+	std::string line;
+	std::string del = ",";
+	while (std::getline(inputFile, line)) { // read line from file
+		std::string firstname, lastname;
+		size_t pos;
+		while ((pos = line.find(del)) != std::string::npos) { // split name/last name by delimiter "del"
+			firstname = line.substr(0, pos);
+			line.erase(0, pos + del.length());
+			lastname = line;
+		}
+		
+		// Create a STUDENT_DATA object and store the data
+		STUDENT_DATA student;
+		student.firstname = firstname;
+		student.lastname = lastname;
+
+		// push student object into vector space
+		students.push_back(student);
+
+	}
+
+	inputFile.close();
+
 	return 1;
 }
